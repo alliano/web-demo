228ed435c2eb1bc9f6f1b45618a91a5ea0aac1a3
alliano <allianoanoanymous>
2021-12-14 23:37:49 +0700

    save tugas

---
+++ script/main.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+    $('.menu-toggle input').click(function () {
+        $('.nav-slide').slideToggle(2000);
+    });
+
+    $('.nav-slide .list-slide ul li a').click(function (args) {
+        
+        let list = $('.nav-slide .list-slide ul li a');
+        const data = $('.nav-slide .list-slide ul li');
+        
+        $('.nav-slide .list-slide ul li a').removeClass("active");
+        $(this).addClass("active");
+
+        for (let i = 0; i < list.length; i++) {
+            $(list[i]).click(function () {
+                if (i === 0) {
+                    localStorage.setItem('page', 'home');
+                     window.location.href = `home.html`
+                } else {
+                    if (i === 1) {
+                        localStorage.setItem('page', 'contac');
+                        window.location.href = `contac.html`
+                    } else {
+                        if (i === 2) {
+                            localStorage.setItem('page', 'about');
+                            window.location.href = `About.html`
+                        } else {
+                            if (i === 3) {
+                                localStorage.setItem('page', 'courses');
+                                window.location.href = `courses.html`
+                            } else {
+                                if (i === 4) {
+                                    localStorage.setItem('page', 'sitemap');
+                                    window.location.href = `sitemap.html`
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+            
+        }
+        
+        args.preventDefault(); 
+
+    });
+
+
+    /////////////////////////////////
+    ////////VIDIO////////////////////
+    /////////////////////////////////
+
+
+
+    $.ajax({
+        type: "get",
+        url: "https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails,player&id=PLFIM0718LjIWXagluzROrA-iBY9eeUt4w&key=AIzaSyCsy7fIaTA90lta3gvp1FFsgiVdHYv-8I8",
+        data: "json",
+        success: function (response) {
+            console.log(response.items[0]);
+
+            $('#vidio').html(response.items[0].player.embedHtml);
+            // $('.card p').tex()
+        }
+    });
+
+
+    $('#vidio').css({
+        "width" : "100%",
+        "height": "100%",
+        "border" : "1px solid #000"
+    })
+    $('.play-vidio iframe').css({
+        "width" : "800px%",
+        "height": "700px",
+        "border" : "2px solid aqua"
+    })
+
+
+    ////////////////////////
+    //////END VIDIO////////
+    ///////////////////////
+
+
+    $('.jumbo #login').css({
+        "cursor": "pointer"
+    });
+    $('.jumbo #login').click(function () {
+        window.location.href = "login.html"
+    })
+    $('.jumbo #sigin').click(function () {
+        window.location.href = "rengister.html"
+    })
+
+
+});
+
+
---
+++ script/script.js
@@ -0,0 +1,90 @@
+
+// let hash1 = hash('aku');
+
+// console.log(hash1);
+
+// async function hash(string) {
+    //   const utf8 = new TextEncoder().encode(string);
+    //   const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+    //   const hashArray = Array.from(new Uint8Array(hashBuffer));
+    //   const hashHex = hashArray
+    //     .map((bytes) => bytes.toString(16).padStart(2, '0'))
+    //     .join('');
+    //   return hashHex;
+    // }
+    
+    (function returnHeadEl() {
+          const head = document.getElementsByTagName('head');
+        const newHeadElmts = `<meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+                                <link rel="preconnect" href="https://fonts.googleapis.com">
+                                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                                <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200&display=swap" rel="stylesheet">
+                                <link rel="preconnect" href="https://fonts.googleapis.com">
+                                <link rel="preconnect" href="https://fonts.gstatic.com"     crossorigin>`;
+        
+        const afterTitle =      ` <!-- font logo -->
+        
+                                <link rel="preconnect" href="https://fonts.googleapis.com">
+                                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                                <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600&display=swap" rel="stylesheet"> `;
+         
+        })();
+
+        (function () {
+            const header = document.getElementsByTagName('header');
+            const returnElements = `
+            <nav>
+                <div class="core">
+                    <div id="logo"></div>
+                    <h1 id="nama">ABC CENTER</h1>
+                </div>
+                <div class="menu-toggle">
+                    <input type="checkbox">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="nav-slide">
+                    <div class="list-slide">
+                        <ul>
+                            <li><a class="${localStorage.getItem('page') === 'home' ? 'active' : ''} home" >Home</a></li>
+                            <li><a class="${localStorage.getItem('page') === 'contac' ? 'active' : ''} contac">Contact</a></li>
+                            <li><a class="${localStorage.getItem('page') === 'about' ? 'active' : ''} about">About</a></li>
+                            <li><a class="${localStorage.getItem('page') === 'courses' ? 'active' : ''}  courses" href="courses">Courses</a></li>
+                            <li><a href="#">Sitemap</a><are/li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>`;
+            header[0].innerHTML = returnElements;
+        })();
+        
+        (function () {
+            const footer = document.getElementsByTagName('footer');
+            const ell = `<div class="ket">
+            <ul>
+            <h3>SiteMap</h3>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Sitemap</a></li>
+            <li><a href="#">Privacy policy</a></li>
+            <li><a href="#">Sechdule</a></li>
+            <li><a href="#">Courses</a></li>
+            </ul>
+            <ul>
+            <h3>About developer</h3>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Github</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Tweeter</a></li>
+            </ul>
+            </div>
+            <div class="copyright"><h2>&copy;copyright2021</h2></div>`;
+            footer[0].innerHTML = ell;
+            
+        })();
+        AOS.init();
