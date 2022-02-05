f6cddcd2a991b89b50c5274e985f4b493c0d60fc
alliano <allianoanonymous@gmail.com>
2022-02-05 01:02:09 -0500

    repair animation

--- script/AuthAdmin.js
+++ script/AuthAdmin.js
@@ -14,11 +14,13 @@ const passwordA = document.getElementById('password');
 
 
 const auth = JSON.parse(window.localStorage.getItem("admin"));
-
 document.addEventListener("click", function (args) {
+    console.log(userNamaeA.value !== auth[0].name ? "iye":"kaga");
+    console.log(EmailA.value === auth[0].email ? "iye":"kaga");
+    console.log(passwordA.value === auth[0].password ? "iye":"kaga");
     if (args.target.classList.contains("LoginAdmin")) {
-        if (userNamaeA !== auth.name && EmailA !== auth.email && passwordA !== auth.password) alert("password or email wrong");
-        if (userNamaeA === auth.name && EmailA === auth.email && passwordA === auth.password) alert("weelcome admin"); window.location.href = "/admin.html";
+        if (userNamaeA.value !== auth[0].name && EmailA.value !== auth[0].email && passwordA.value !== auth[0].password)alert("password or email wrong");
+        if (userNamaeA.value === auth[0].name && EmailA[0].value === auth[0].email && passwordA.value === auth[0].password)alert("weelcome admin"); window.location.href = "/admin.html";
         
     }
 
