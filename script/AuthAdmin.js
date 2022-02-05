9fc6001a8c51fada16e6b6dd0bafba45c094ce64
alliano <allianoanonymous@gmail.com>
2022-02-05 01:38:57 -0500

    repair animation

--- script/AuthAdmin.js
+++ script/AuthAdmin.js
@@ -17,10 +17,17 @@ const auth = JSON.parse(window.localStorage.getItem("admin"));
 document.addEventListener("click", function (args) {
     console.log(userNamaeA.value !== auth[0].name ? "iye":"kaga");
     console.log(EmailA.value === auth[0].email ? "iye":"kaga");
-    console.log(passwordA.value === auth[0].password ? "iye":"kaga");
+    console.log(passwordA.value === auth[0].password ? "iye" : "kaga");
+    
+    
     if (args.target.classList.contains("LoginAdmin")) {
-        if (userNamaeA.value !== auth[0].name && EmailA.value !== auth[0].email && passwordA.value !== auth[0].password)alert("password or email wrong");
-        if (userNamaeA.value === auth[0].name && EmailA.value === auth[0].email && passwordA.value === auth[0].password)alert("weelcome admin"); window.location.href = "/admin.html";
+        if (userNamaeA.value !== auth[0].name && EmailA.value !== auth[0].email && passwordA.value !== auth[0].password) {
+            alert("password or email wrong");
+        }
+        if (userNamaeA.value === auth[0].name && EmailA.value === auth[0].email && passwordA.value === auth[0].password) {
+            alert("weelcome admin");
+            window.location.href = "/admin.html";
+        }
         
     }
 
